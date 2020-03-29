6d260bca7780fd9bb032d35f852ff1402c75c6c9
thepassle <pascalschilp@gmail.com>
2020-03-29 13:26:21 +0200

    chore: add base options

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const Terser = require('terser');
 const { createScript } = require('@open-wc/building-utils');
 const { append, predicates, query } = require('@open-wc/building-utils/dom5-fork');
 
-
 /**
  * @param {string} htmlString
  * @param {string} scope
+ * @param {string} base
  * @returns {string}
  */
-function applyServiceWorkerRegistration(htmlString, scope) {
+function applyServiceWorkerRegistration(htmlString, scope, base) {
   const addScope = !!scope;
+  const addbase = !!base;
   const documentAst = parse(htmlString);
   const body = query(documentAst, predicates.hasTagName('body'));
   const swRegistration = createScript(
@@ -19,7 +20,7 @@ function applyServiceWorkerRegistration(htmlString, scope) {
     if ('serviceWorker' in navigator) {
       window.addEventListener('load', function() {
         navigator.serviceWorker
-          .register('./sw.js'${addScope ? `,{ scope: '.${scope}' }` : ''})
+          .register('./${addbase ? base : ''}sw.js'${addScope ? `,{ scope: '.${scope}' }` : ''})
           .then(function() {
             console.log('ServiceWorker registered.');
           })
@@ -36,16 +37,31 @@ function applyServiceWorkerRegistration(htmlString, scope) {
 };
 
 /**
+ * The complete Triforce, or one or more components of the Triforce.
+ * @typedef {Object} Options
+ * @property {String} [htmlFileName='index.html'] htmlFileName
+ * @property {String} [scope=''] scope
+ * @property {boolean} [base=''] - useful if you're using a <base> tag, example: base: 'foo/'
+ */
+
+/**
  * Takes the name of the index.html, and appends a minified service worker registration to the end of the document body
  * @param {String} [htmlFileName='index.html'] htmlFileName
  * @param {String} [scope=''] scope
  */
-module.exports = function applySwRegistration(htmlFileName = 'index.html', scope = '') {
+module.exports = function applySwRegistration(options) {
+  const _opts = { 
+    htmlFileName: 'index.html',
+    scope: '',
+    base: '',
+    ...(options || {})
+  };
+
   return {
     name: 'rollup-plugin-inject-service-worker',
     generateBundle(_, bundle) {
-      const htmlSource = bundle[htmlFileName].source;
-      bundle[htmlFileName].source = applyServiceWorkerRegistration(htmlSource, scope);
+      const htmlSource = bundle[_opts.htmlFileName].source;
+      bundle[_opts.htmlFileName].source = applyServiceWorkerRegistration(htmlSource, _opts.scope, _opts.base);
     },
   }
 }
